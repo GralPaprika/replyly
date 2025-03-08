6187e1b0dc56d955479b048270dccd45c58435c8
CarlosFR4 <cfr4123@gmail.com>
2025-03-08 00:19:53 -0600

    [auth] remove validator

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,15 @@
-import {NextRequest, NextResponse} from "next/server";
-import {HttpResponseCode} from "@/lib/common/models/HttpResponseCode";
-
-const API_HEADER = 'x-api-key'
-const RESPONSE_MESSAGE = 'Unauthorized'
-
-export function middleware(request: NextRequest) {
-  const apiKey = process.env.API_KEY
-  const requestApiKey = request.headers.get(API_HEADER)
-
-  if (!requestApiKey || requestApiKey !== apiKey) {
-    return NextResponse.json({error: RESPONSE_MESSAGE}, {status: HttpResponseCode.Unauthorized})
-  }
-  return NextResponse.next()
-}
+// import {NextRequest, NextResponse} from "next/server";
+// import {HttpResponseCode} from "@/lib/common/models/HttpResponseCode";
+//
+// const API_HEADER = 'x-api-key'
+// const RESPONSE_MESSAGE = 'Unauthorized'
+//
+// export function middleware(request: NextRequest) {
+//   const apiKey = process.env.API_KEY
+//   const requestApiKey = request.headers.get(API_HEADER)
+//
+//   if (!requestApiKey || requestApiKey !== apiKey) {
+//     return NextResponse.json({error: RESPONSE_MESSAGE}, {status: HttpResponseCode.Unauthorized})
+//   }
+//   return NextResponse.next()
+// }
