743e013ddef605f722203d083544d3ba471f5f75
CarlosFR4 <cfr4123@gmail.com>
2025-03-11 00:07:41 -0600

    [bot] send country code to bot,db changes

---
+++ db/migrations/0010_great_hiroim.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "secretaries" ADD COLUMN "country_code" text;--> statement-breakpoint
+ALTER TABLE "whatsapp" ADD COLUMN "phone_number" text;--> statement-breakpoint
+ALTER TABLE "whatsapp" ADD COLUMN "country_code" text;
